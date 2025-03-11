1b23c67cecf1915d678bcbc7845f8953792ca214
M33dz <contact@mehdy-elm.com>
2025-03-11 17:19:23 +0100

    scene added

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { components } from '~/slices'
+import { Scene } from '../scenes/BlobSceneClass';
+import gsap from 'gsap';
 
 const prismic = usePrismic();
+
 const { data: page } = await useAsyncData('index', () =>
   prismic.client.getByUID('page', 'home', {
     fetchLinks: [
@@ -18,13 +21,68 @@ useSeoMeta({
     ogDescription: page.value?.data.meta_description,
     ogImage: computed(() => prismic.asImageSrc(page.value?.data.meta_image)),
 });
+
+const blob = ref(null);
+
+onMounted(() => {
+
+  if (!blob.value) return;
+            
+  const sceneInstance = new Scene({
+      domElement: blob.value // Pass native DOM element
+  });
+
+  const animMesh = sceneInstance.mesh;
+
+  let g1Tl = gsap.timeline({
+      clearProps: true,
+      scrollTrigger: {
+          trigger: '.hello',
+          start: "top 60%",
+          scrub: 2
+      }
+  });
+
+  g1Tl.to(animMesh.rotation, {
+      x: 0.5,
+      y: -1
+  });
+
+  g1Tl.to(sceneInstance.camera.position, {
+      x: 3,
+      z: 4.5
+  }, '-= 1');
+
+
+  let g2Tl = gsap.timeline({
+      clearProps: true,
+      scrollTrigger: {
+          trigger: '.techno',
+          start: "top 80%",
+          scrub: 2
+      }
+  });
+  
+  g2Tl.to(animMesh.material.uniforms.uNoiseDensity, {
+      value: 2.8
+  }, '-= 1');
+
+  g2Tl.to(sceneInstance.camera.position, {
+      x: 7,
+      y: -3,
+      z: -2
+  }, '-= 1');
+});
 </script>
 
 <template>
-  <SliceZone
-    wrapper="main"
-    :slices="page?.data.slices ?? []"
-    :components="components"
-    id="home"
-  />
+  <div>
+    <div id="gl-stuff" ref="blob"></div>
+    <SliceZone
+      wrapper="main"
+      :slices="page?.data.slices ?? []"
+      :components="components"
+      id="home"
+    />
+  </div>
 </template>
